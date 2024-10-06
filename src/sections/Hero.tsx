3b47d4c930ef574d0d933286a4044ede1dccb68e
King Grey <lohitsaidev@gmail.com>
2024-10-06 19:18:23 +0530

    Added stars

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,8 @@
 import ArrowDown from '@/assets/icons/arrow-down.svg';
+import StarIcon from '@/assets/icons/star.svg';
 import grainImage from '@/assets/images/grain.jpg';
-import memoji from '@/assets/images/memoji-computer.png';
+import memojiImage from '@/assets/images/memoji-computer.png';
+import HeroOrbit from '@/components/HeroOrbit';
 import Image from 'next/image';
 
 export const HeroSection = () => {
@@ -12,17 +14,32 @@ export const HeroSection = () => {
           backgroundImage: `url(${grainImage.src})`,
         }}
       ></div>
+
       {/* Ringa Ringa Rings */}
       <div className="size-[620px] hero-ring"></div>
       <div className="size-[830px] hero-ring"></div>
       <div className="size-[1020px] hero-ring"></div>
       <div className="size-[1220px] hero-ring"></div>
+
+      {/* Stars orbiting my hero section */}
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className="size-48 text-emerald-300" />
+      </HeroOrbit>
+
+      <HeroOrbit size={550} rotation={20}>
+        <StarIcon className="size-12 text-emerald-300" />
+      </HeroOrbit>
+
+      <HeroOrbit size={590} rotation={98}>
+        <StarIcon className="size-8 text-emerald-300" />
+      </HeroOrbit>
+
       <div className="container">
         {/* Container for the Image */}
         <div className="flex flex-col items-center">
           <Image
             className="size-[100px]"
-            src={memoji}
+            src={memojiImage}
             alt="Person Looking at the laptop"
           />
           <div className="bg-gray-950 border border-gray-800 py-1.5 px-4 inline-flex items-center gap-4 rounded-lg">
