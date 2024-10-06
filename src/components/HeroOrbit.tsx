9413727e45f98fde48465c525f3955b76da3311b
King Grey <lohitsaidev@gmail.com>
2024-10-06 19:37:22 +0530

    Completed Hero section

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -9,7 +9,7 @@ const HeroOrbit = ({
     // Stars
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <div
-        className=""
+        className="flex items-start justify-start"
         style={{
           height: `${size}px`,
           width: `${size}px`,
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,38 +1,80 @@
+import Image from 'next/image';
+
 import ArrowDown from '@/assets/icons/arrow-down.svg';
+import SparkleIcon from '@/assets/icons/sparkle.svg';
 import StarIcon from '@/assets/icons/star.svg';
 import grainImage from '@/assets/images/grain.jpg';
 import memojiImage from '@/assets/images/memoji-computer.png';
+
 import HeroOrbit from '@/components/HeroOrbit';
-import Image from 'next/image';
 
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0">
-      <div
-        className="absolute inset-0 -z-30 opacity-5"
-        style={{
-          backgroundImage: `url(${grainImage.src})`,
-        }}
-      ></div>
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
 
-      {/* Ringa Ringa Rings */}
-      <div className="size-[620px] hero-ring"></div>
-      <div className="size-[830px] hero-ring"></div>
-      <div className="size-[1020px] hero-ring"></div>
-      <div className="size-[1220px] hero-ring"></div>
+        {/* Ringa Ringa Rings */}
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[830px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
 
-      {/* Stars orbiting my hero section */}
-      <HeroOrbit size={800} rotation={-72}>
-        <StarIcon className="size-48 text-emerald-300" />
-      </HeroOrbit>
+        {/* Stars orbiting my hero section */}
+        <HeroOrbit size={800} rotation={-72}>
+          <StarIcon className="size-48 text-emerald-300" />
+        </HeroOrbit>
 
-      <HeroOrbit size={550} rotation={20}>
-        <StarIcon className="size-12 text-emerald-300" />
-      </HeroOrbit>
+        {/* Stars orbiting my hero section */}
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
 
-      <HeroOrbit size={590} rotation={98}>
-        <StarIcon className="size-8 text-emerald-300" />
-      </HeroOrbit>
+        {/* Stars orbiting my hero section */}
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+
+        {/* Sparkle */}
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+
+        {/* Sparkle */}
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+
+        {/* Sparkle */}
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+
+        {/* Sparkle */}
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+
+        {/* Circle */}
+        <HeroOrbit size={720} rotation={85}>
+          <div className="size-3 bg-emerald-300/20 rounded-full"></div>
+        </HeroOrbit>
+
+        {/* Circle */}
+        <HeroOrbit size={520} rotation={-41}>
+          <div className="size-3 bg-emerald-300/20 rounded-full"></div>
+        </HeroOrbit>
+
+        {/* Circle */}
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-3 bg-emerald-300/20 rounded-full"></div>
+        </HeroOrbit>
+      </div>
 
       <div className="container">
         {/* Container for the Image */}
