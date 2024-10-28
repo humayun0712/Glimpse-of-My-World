5bad05bfab950aee3fb324982503cffb8eff051d
King Grey <lohitsaidev@gmail.com>
2024-10-28 18:08:17 +0530

    Completed Animation for Projects

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,13 @@
 import grainImage from '@/assets/images/grain.jpg';
-import { PropsWithChildren } from 'react';
+import { ComponentPropsWithoutRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-type CardProps = PropsWithChildren<{ className?: string }>;
+type CardProps = ComponentPropsWithoutRef<'div'>;
 
-const Card = ({ children, className }: CardProps) => {
+const Card = ({ children, className, ...props }: CardProps) => {
   return (
     <div
+      {...props}
       className={twMerge(
         "bg-gray-800 rounded-3xl relative after:content-[''] after:absolute after:inset-0 z-0 after:z-10 overflow-hidden after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
         className
