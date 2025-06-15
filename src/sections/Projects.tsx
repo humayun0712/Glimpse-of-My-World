3e6bac9a0c2af5fd6e1c71ef04519b90c934e0bd
Vijay Rama Raju <vijay.siruvuru@gmail.com>
2025-06-15 11:04:26 +0530

    Update Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -9,7 +9,7 @@ import gamingWebsiteImage from "@/assets/images/Gaming.png";
 import searchEngineImage from "@/assets/images/search engine.png";
 import pingPongImage from "@/assets/images/pingpong.png";
 import ticTacToeImage from "@/assets/images/Tictactoe.png";
-import quantumimage from "@assets/images/quantumwarrior.png";
+import quantumImage from "@/assets/images/quantumwarrior.png";
 
 const portfolioProjects = [
   {
@@ -82,7 +82,7 @@ const portfolioProjects = [
       { title: "Easy way to learn topics" },
     ],
     link: "https://github.com/Vijay-1289/quantum-warrior-quest-saga",
-    image: quantumimage,
+    image: quantumImage,
   }
 ];
 
